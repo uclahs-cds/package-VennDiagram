24e4f90dcc62e24517dd46dc7b563cbe53da9851
dwang <dwang@noemail.ucla.edu>
2015-08-25 13:40:13 +0000

    Removed more Unicode characters so that the compatibility note will finally go away
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@46341 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/partitionText.R
+++ R/partitionText.R
@@ -47,9 +47,9 @@ make.truth.table <- function(x)
 # section:  Details
 # If force.unique is FALSE, then there are two supported methods of grouping categories with duplicated elements in common.
 # If hierarchical is FALSE, then any common elements are gathered into a pool. So if
-# x <- list(a = c(1,1,2,2,3,3), b=c(1,2,3,4,4,5), c=c(1,4)) then (b intersect c)∖(a) would contain
-# three 4's. Since the 4's are pooled, (b)∖(a union c) contains no 4's.
-# If hierachical is TRUE, then (b intersect c)∖(a) would contain one 4.Then (b)∖(a union c) cotains one 4.
+# x <- list(a = c(1,1,2,2,3,3), b=c(1,2,3,4,4,5), c=c(1,4)) then (b intersect c)/(a) would contain
+# three 4's. Since the 4's are pooled, (b)/(a union c) contains no 4's.
+# If hierachical is TRUE, then (b intersect c)/(a) would contain one 4.Then (b)/(a union c) cotains one 4.
 # 
 # author:  Richard Cotton.
 # seealso:  [VennDiagram]{venn.diagram}, {make.truth.table}
